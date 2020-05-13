3f1c9ae3d811a0afb7e141a42c5ab8a95a1704a0
Jen E <jenevans3ten@gmail.com>
2020-05-13 13:42:02 -0400

    initial commit

---
+++ Greet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
