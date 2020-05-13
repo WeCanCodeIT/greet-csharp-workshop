e633ffc68f35a1f0192dffab645e6758ccf53fa6
Jen E <jenevans3ten@gmail.com>
2020-05-13 14:07:30 -0400

    added starter tests

--- Greet.Tests/ProgramTests.cs
+++ Greet.Tests/ProgramTests.cs
@@ -5,9 +5,41 @@ namespace Greet.Tests
 {
     public class ProgramTests
     {
+        // 1: Write a method Greet(name) that interpolates name in a simple greeting. 
+        // For example, when name is "Bob", the method should return a string
+        // "Hello, Bob.".
+
         [Fact]
-        public void Test1()
+        public void Greet_Should_Return_Greeting()
         {
+            string greeting = Program.Greet("Bob");
+
+            Assert.Equal("Hello, Bob", greeting);
+
+        }
+
+        // 2: Handle nulls by introducing a stand-in. 
+        // For example, when name is null, then the method should
+        // return the string "Hello, my friend."
+
+        [Fact]
+        public void Greet_Should_Return_myfriend_for_null()
+        {
+            string greeting = Program.Greet("");
+
+            Assert.Equal("Hello, my friend", greeting);
+        }
+
+        // 3: Handle shouting. When name is all uppercase, then
+        // the method should shout back to the user. For example, 
+        // when name is "JERRY" then the method should return the string "HELLO JERRY!"
+
+        [Fact]
+        public void Greet_Should_Return_Shout_for_All_Caps()
+        {
+            string greeting = Program.Greet("JERRY");
+
+            Assert.Equal("HELLO JERRY!", greeting);
 
         }
     }
